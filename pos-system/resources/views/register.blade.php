a0d146379bed15d8e4f1d3a7254003e6728f438a
Faith <felominofaith@gmail.com>
2026-08-07 10:53:09 +0800

    bootstrap set up
    added bootstrap to app.js and view headers

--- pos-system/resources/views/register.blade.php
+++ pos-system/resources/views/register.blade.php
@@ -1,1 +1,19 @@
-register page here
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reigister</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    
+
+
+
+
+
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
--- pos-system/resources/views/welcome.blade.php
+++ pos-system/resources/views/welcome.blade.php
@@ -1,1 +1,19 @@
-make welcome page here 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    
+
+
+
+
+
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
