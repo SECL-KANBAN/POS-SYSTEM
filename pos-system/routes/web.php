416add1ea41ecbf2da1b90cca8b59fb245d0efbf
loriean <loriejeanumoso2@gmail.com>
2026-08-10 13:56:26 +0800

    web.php

--- pos-system/routes/web.php
+++ pos-system/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return redirect()->route('register');
 });
 
 Route::get('/dashboard', function () {
