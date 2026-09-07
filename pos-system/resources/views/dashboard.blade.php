124dbf3d7ff4e6dd782fada7157856229306a2d9
Faith <felominofaith@gmail.com>
2026-09-07 11:08:50 +0800

    fixing conflict

--- pos-system/resources/views/dashboard.blade.php
+++ pos-system/resources/views/dashboard.blade.php
@@ -310,8 +310,6 @@
                                 </button>
                             </form>
                         </div>
-
-                        
                     </div>
                 </div>
             </div>
